9d709d47b7e148980df7eddf808a130a18e762d8
eehkani <82057076+eehkani@users.noreply.github.com>
2021-09-29 14:14:20 +0900

    input
    ボタンを押したら、押した文字がinputに表示されるようにしました

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,11 @@
-const Input = () => {
+import React, {useState} from "react";
+
+const Input = (value) => {
+ 
   return (
     <>
       {/* <input className="input" type="text" value="input" /> */}
-      <input className="input" type="text" />
+      <input className="input" type="text" value = {value.index} />
     </>
   )
 }
